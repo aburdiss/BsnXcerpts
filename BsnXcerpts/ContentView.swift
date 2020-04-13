dfcfcf453f9537ed4a8379832e0fa62b5c55818c
Alexander Burdiss <aburdiss@gmail.com>
2020-04-12 21:06:35 -0400

    Initial Commit

---
+++ BsnXcerpts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BsnXcerpts
+//
+//  Created by Alex Burdiss on 4/12/20.
+//  Copyright © 2020 Alex Burdiss. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
